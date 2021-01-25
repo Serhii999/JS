8dd62e5d697453fe23a6b714a909b209431370e1
Karenov <serhii.korzhov@nure.ua>
2021-01-25 15:10:20 +0200

    Novoedz

---
+++ js1/dz2.js
@@ -0,0 +1,29 @@
+let x=10;
+
+var res=0;
+if (x>=0){
+    console.log("Число положительное")}
+else{
+    console.log("Число отрицательное")
+    }
+
+
+let nums= [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+for (let i=0; i<nums.length; i++){
+    res+= nums[i];
+
+}
+console.log(res);
+
+
+var numbers = [ 254, 115, 78, 25, 91, 45, 37 ];
+var last=0;
+for (let i= 0; i<numbers.length; i++){
+    if (numbers[i]>50){
+        console.log(numbers[i])
+    }
+}
+
+
+
